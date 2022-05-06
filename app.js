bac12a43353dfcd47ca085dbfcf20290aa5a0597
Nicola Derocchi <nicoladerocchi@fedabo.com>
2022-05-06 17:49:01 +0200

    file pre deploy

--- app.js
+++ app.js
@@ -464,6 +464,11 @@ function getDayInMonth(m, a) {
 
 //LISTEN ------------------------------------
 
-app.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
     console.log("Server running");
 })
